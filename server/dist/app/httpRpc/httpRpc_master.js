1cf587b76ac8643bbba12b15eb9eb0cefb12dadb
huangjizhong <1124831983@qq.com>
2022-09-24 09:17:26 +0800

    fdsa

--- server/dist/app/httpRpc/httpRpc_master.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * version: 1.0.0
- * 这是测试项目的声明文件
- */
-Object.defineProperty(exports, "__esModule", { value: true });
--- server/node_modules/mydog/template/server/src/config/sys/route.ts
+++
@@ -1,3 +0,0 @@
-export default [
-    "connector.main.ping",  // ping测试
-]
